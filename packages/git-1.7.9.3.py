5c2af1890a3d78905ff8d0e11345666aa706372f
thiesgehrmann <thiesgehrmann@gmail.com>
2016-12-01 13:54:51 +0100

    Added perl dep for git

--- packages/git-1.7.9.3.py
+++
@@ -1,9 +0,0 @@
-from package import *
-
-class git(Package):
-    dependencies = ["python",'openssl','zlib','curl']
-
-    fetch="http://git-core.googlecode.com/files/git-1.7.9.3.tar.gz"
-
-    install="make PYTHON_PATH=%(prefix)s/bin/python NO_EXPAT=YesPlease NO_TCLTK=YesPlease PERL_PATH=%(prefix)s/bin/perl prefix=%(prefix)s all install"
-
