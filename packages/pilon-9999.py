2f04a3a02197dda43f83df373429a47e51cee88e
thiesgehrmann <thiesgehrmann@gmail.com>
2016-12-14 08:27:02 +0100

    updated pilon wrapper file to allow for JVM commands

--- packages/pilon-9999.py
+++ packages/pilon-9999.py
@@ -14,8 +14,9 @@ class pilon(Package):
       c = """./build.sh
              mkdir -p %(prefix)s/opt/pilon/
              find | grep "pilon.*one-jar.jar" | xargs -i cp {} %(prefix)s/opt/pilon-9999.jar
-             echo -en '#!/bin/sh\njava -jar %(prefix)s/opt/pilon-9999.jar "$@"' > %(prefix)s/bin/pilon
-             chmod +x %(prefix)s/bin/pilon"""
+             echo -en '#!/bin/sh\njava_args="$1";\nshift;\nif [ "$java_args" == "-" ]; then\n  java_args="";\nfi\njava $java_args -jar %(prefix)s/opt/pilon-9999.jar "$@"' > %(prefix)s/bin/pilon
+             chmod +x %(prefix)s/bin/pilon
+             echo 'BE AWARE OF SPECIAL INPUT FORMAT. FIRST PARAMETER TO pilon ARE JVM ARGUMENTS. SEE %(prefix)s/bin/pilon'"""
       for cmd in c.split('\n'):
         runCommand(self.fillVars(c))
 
