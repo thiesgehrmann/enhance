4a8cc39b3d70af407abe44255198735eb7b51ea2
thiesgehrmann <thiesgehrmann@gmail.com>
2016-12-15 15:35:24 +0100

    Fixed Blast Makefile so make install works

--- packages/ncbi_blast-2.5.0.py
+++ packages/ncbi_blast-2.5.0.py
@@ -5,3 +5,9 @@ class ncbi_blast(MakePackage):
     fetch='ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.5.0/ncbi-blast-2.5.0+-src.tar.gz'
     workdir="ncbi-blast-2.5.0+-src/c++/"
     config="./configure --with-mt --prefix=%(prefix)s --without-debug"
+
+    def install(self):
+      c = """sed -i.bak -e 's:$(INSTALL) -m 644 $(incdir)/[*] $(pincludedir):$(INSTALL) -m 644 $(incdir)/common/* $(incdir)/*.h $(pincludedir):' Makefile
+             make install"""
+      for cmd in c.split('\n'):
+        runCommand(self.fillVars(c))
